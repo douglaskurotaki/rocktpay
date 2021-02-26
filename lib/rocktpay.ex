40df49fa0656a3c999ff0d76ff2b96acc176a226
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-02-26 01:33:54 -0300

    Create route transaction

--- lib/rocktpay.ex
+++ lib/rocktpay.ex
@@ -1,8 +1,9 @@
 defmodule Rocktpay do
   alias Rocktpay.Users.Create, as: UserCreate
-  alias Rocktpay.Accounts.{Deposit, Withdraw}
+  alias Rocktpay.Accounts.{Deposit, Transaction, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call # chama a funcao call o User.Create
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
--- lib/rocktpay_web/controllers/accounts_controller.ex
+++ lib/rocktpay_web/controllers/accounts_controller.ex
@@ -20,4 +20,12 @@ defmodule RocktpayWeb.AccountsController do
       |> render("update.json", account: account)
     end
   end
+
+  def transaction(conn, params) do
+    with {:ok, %{} = transaction} <- Rocktpay.transaction(params) do
+      conn
+      |> put_status(:ok)
+      |> render("transaction.json", transaction: transaction)
+    end
+  end
 end
